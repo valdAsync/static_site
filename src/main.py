ccb730b89537ea0cc925d5d7be8b1bb4aea114d1
valdAsync <valdasync@proton.me>
2024-09-07 18:08:44 +0300

    html node

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
+from htmlnode import HTMLNode
 from textnode import TextNode
 
 
 def main():
     text_node = TextNode("hello", "text", "https://www.google.com")
     print(text_node)
+    print("#########################")
+
+    html_node = HTMLNode(
+        "div", "hello", [text_node], {"href": "https://www.google.com"}
+    )
+    print(html_node)
+    print(html_node.props_to_html())
 
 
 main()
