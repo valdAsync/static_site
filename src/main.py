6a06a5b4cca8cc559303f43fb88b67f45e629b2d
valdAsync <valdasync@proton.me>
2024-09-07 17:04:44 +0300

    text nodes

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import TextNode
+
+
+def main():
+    text_node = TextNode("hello", "text", "https://www.google.com")
+    print(text_node)
+
+
+main()
---
+++ src/textnode.py
@@ -0,0 +1,25 @@
+text_type_text = "text"
+text_type_bold = "bold"
+text_type_italic = "italic"
+text_type_code = "code"
+text_type_link = "link"
+text_type_image = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, other):
+        if (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        ):
+            return True
+        return False
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
