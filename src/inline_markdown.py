f413cfd417c67c7e11ebb10fcfaab2303e27a77f
valdAsync <valdasync@proton.me>
2024-09-08 12:17:04 +0300

    add final text to node extraction

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -11,6 +11,16 @@ from textnode import (
 )
 
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
+
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
     for node in old_nodes:
