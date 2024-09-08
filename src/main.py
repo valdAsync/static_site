8b222cea5a9300a48e2e969e5551998c904a8f9e
valdAsync <valdasync@proton.me>
2024-09-08 16:58:21 +0300

    copy static

--- src/main.py
+++ src/main.py
@@ -1,24 +1,34 @@
+import os
+import shutil
+
 from htmlnode import HTMLNode, LeafNode
 from textnode import TextNode
 
+dir_path_static = "./static"
+dir_path_public = "./public"
+
+
+def copy_directory(source, destination):
+    if not os.path.exists(destination):
+        os.makedirs(destination)
+
+    for item in os.listdir(source):
+        source_path = os.path.join(source, item)
+        destination_path = os.path.join(destination, item)
+
+        if os.path.isdir(source_path):
+            print(f"Copying directory {source_path} to {destination_path}")
+            copy_directory(source_path, destination_path)
+        else:
+            print(f"Copying file {source_path} to {destination_path}")
+            shutil.copy2(source_path, destination_path)
+
 
 def main():
-    text_node = TextNode("hello", "text", "https://www.google.com")
-    print(text_node)
-    print("#########################")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    html_node = HTMLNode(
-        "div", "hello", [text_node], {"href": "https://www.google.com"}
-    )
-    print(html_node)
-    print(html_node.props_to_html())
-
-    print("#########################")
-
-    leaf_node = LeafNode("p", "This is a paragraph of text.")
-    print(leaf_node.to_html())
-    leaf_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
-    print(leaf_node.to_html())
+    copy_directory(dir_path_static, dir_path_public)
 
 
 main()
