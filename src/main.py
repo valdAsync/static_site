7cf33bb750e0b8930639fe2836653d2a3a9c0ef3
valdAsync <valdasync@proton.me>
2024-09-08 17:01:19 +0300

    copy static to separate file

--- src/main.py
+++ src/main.py
@@ -1,27 +1,12 @@
 import os
 import shutil
 
+from copystatic import copy_directory
 from htmlnode import HTMLNode, LeafNode
 from textnode import TextNode
 
 dir_path_static = "./static"
 dir_path_public = "./public"
-
-
-def copy_directory(source, destination):
-    if not os.path.exists(destination):
-        os.makedirs(destination)
-
-    for item in os.listdir(source):
-        source_path = os.path.join(source, item)
-        destination_path = os.path.join(destination, item)
-
-        if os.path.isdir(source_path):
-            print(f"Copying directory {source_path} to {destination_path}")
-            copy_directory(source_path, destination_path)
-        else:
-            print(f"Copying file {source_path} to {destination_path}")
-            shutil.copy2(source_path, destination_path)
 
 
 def main():
