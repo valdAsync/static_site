3a6a9085871dc074efe5b1d4af81762ef66eb7f0
valdAsync <valdasync@proton.me>
2024-09-08 13:14:50 +0300

    block to block type

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,11 @@
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_olist = "ordered_list"
+block_type_ulist = "unordered_list"
+
+
 def markdown_to_blocks(markdown):
     blocks = []
     for line in markdown.split("\n\n"):
@@ -5,3 +13,24 @@ def markdown_to_blocks(markdown):
             continue
         blocks.append(line.strip())
     return blocks
+
+
+def block_to_block_type(block):
+    if (
+        block.startswith("# ")
+        or block.startswith("## ")
+        or block.startswith("### ")
+        or block.startswith("#### ")
+        or block.startswith("##### ")
+        or block.startswith("###### ")
+    ):
+        return block_type_heading
+    if block.startswith("```") and block.endswith("```"):
+        return block_type_code
+    if block.startswith(">"):
+        return block_type_quote
+    if block.startswith("* ") or block.startswith("- "):
+        return block_type_ulist
+    if block[0].isdigit() and block[1] == ".":
+        return block_type_olist
+    return block_type_paragraph
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,13 @@
 import unittest
 
 from markdown_blocks import (
+    block_to_block_type,
+    block_type_code,
+    block_type_heading,
+    block_type_olist,
+    block_type_paragraph,
+    block_type_quote,
+    block_type_ulist,
     markdown_to_blocks,
 )
 
@@ -48,3 +55,17 @@ This is the same paragraph on a new line
                 "* This is a list\n* with items",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), block_type_heading)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), block_type_code)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), block_type_quote)
+        block = "* list\n* items"
+        self.assertEqual(block_to_block_type(block), block_type_ulist)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), block_type_olist)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), block_type_paragraph)
